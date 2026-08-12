d651459289f1f81ed97fdc3cdd7e32dea9ab0ef4
menn814 <ddm044915@gmail.com>
2026-08-13 02:03:17 +0300

    Add files via upload

---
+++ main(9).py
@@ -0,0 +1,15 @@
+n = int(input())
+
+student_marks = {}
+
+for _ in range(n):
+    name, *marks = input().split()
+    student_marks[name] = list(map(float, marks))
+
+query_name = input()
+
+marks = student_marks[query_name]
+
+average = sum(marks) / len(marks)
+
+print(f"{average:.2f}")
