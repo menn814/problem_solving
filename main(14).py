4f5949b41da4e397ed0ab473b21e18b15c4024e1
menn814 <ddm044915@gmail.com>
2026-08-13 02:20:23 +0300

    Add files via upload

---
+++ main(14).py
@@ -0,0 +1,18 @@
+def swap_case(s):
+    result = ""
+
+    for char in s:
+        if char.islower():
+            result += char.upper()
+        elif char.isupper():
+            result += char.lower()
+        else:
+            result += char
+
+    return result
+    return
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
