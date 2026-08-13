5fc275c2bff9f34c1f0fc27768777fb8236ae1b0
menn814 <ddm044915@gmail.com>
2026-08-13 03:00:07 +0300

    Add files via upload

---
+++ main(25).py
@@ -0,0 +1,22 @@
+def minion_game(string):
+    kevin = 0
+    stuart = 0
+
+    vowels = "AEIOU"
+
+    for i in range(len(string)):
+        if string[i] in vowels:
+            kevin += len(string) - i
+        else:
+            stuart += len(string) - i
+
+    if kevin > stuart:
+        print("Kevin", kevin)
+    elif stuart > kevin:
+        print("Stuart", stuart)
+    else:
+        print("Draw")
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
